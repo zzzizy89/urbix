d9818cc5b28599d63f8b1fcdef0ec7c5f9592bf9
EzequielMonteverde <cristianmonteverde@alumnos.itr3.edu.ar>
2023-08-09 16:14:16 -0300

    base de datos
    cambio

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -29,7 +29,7 @@ class Database extends Config
         'hostname' => 'localhost',
         'username' => 'root',
         'password' => '',
-        'database' => 'codeigniter4',
+        'database' => 'keytech',
         'DBDriver' => 'MySQLi',
         'DBPrefix' => '',
         'pConnect' => false,
