ff6b49782429b05d8478e0d1d1857a66c58f1f8c
tiagocomba <tiagocomba89@proton.me>
2023-08-14 11:14:51 -0300

    cambio de public

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,8 +6,8 @@ class Home extends BaseController
 {
     public function index()
     {
-        echo view('common/header');
-        return view('users/home');
-        echo view('common/header');
+        
+        return view('inicio/vista');
+        
     }
 }
