601138a6d3243e34a5874457b42b1197b72a0db9
EzequielMonteverde <cristianmonteverde@alumnos.itr3.edu.ar>
2023-08-23 17:42:47 -0300

    created_at y updated_at

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -11,14 +11,14 @@ class UserModel extends Model
     protected $useAutoIncremental = true;
 
     protected $returnType = 'object';
-    protected $useSoftDeletes = true;
+    //protected $useSoftDeletes = true;
 
     protected $allowedFields = ['name','email','password','created_at','updated_at'];
 
     protected $useTimestamps = false;
     protected $createdField = 'created_at';
     protected $updatedField = 'updated_at';
-    protected $deletedField = 'deleted_at';
+
 
     protected $validationRules = [];
     protected $validationMessages = [];
