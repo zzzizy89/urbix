7b79ffb23784818560fcf739de67796f3a85e72b
ᴄᴏᴍʙᴀ.ᴄᴏᴅᴇ <tiagocomba89@proton.me>
2023-10-15 23:29:03 -0300

    dashboard no finish

--- app/Views/main/form/dashboard.php
+++ app/Views/main/form/dashboard.php
@@ -4,128 +4,65 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/dashboard.css">
-	<link rel="stylesheet" href="../css/dashboard.css">
-    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <title>Document</title>
+    <!-- Agrega los enlaces a tus archivos CSS y scripts JS aquí -->
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/dashboard.css');?>">
+    <title>Perfil de Usuario</title>
 </head>
 <body>
 
-<!------ Include the above in your HEAD tag ---------->
-
-<div class="container emp-profile">
-
-         
-                <div class="row">
-                    <div class="col-md-4">
-                        <div class="profile-img">
-                            <img src="../img/productos/gato.jpg"  alt=""/>
-                            <div class="file btn btn-lg btn-primary">
-                                Change Photo
-                                <input type="file" name="file"/>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="profile-head">
-                                    <h5>
-                                    <?= session('user')->name; ?>
-                                    </h5>
-                                    <h6>
-                                        Web Developer and Designer
-                                    </h6>
-                                    <p class="proile-rating">RANKINGS : <span>8/10</span></p>
-                            <ul class="nav nav-tabs" id="myTab" role="tablist">
-                                <li class="nav-item">
-                                    <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">About</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                   
-                    <div class="col-md-2">
-                    <input type="submit" class="profile-edit-btn" name="btnAddMore" value="Edit Profile"/>
-                  
-                    <a href="<?= base_url('logout') ?>"><button type="submit" class="btn">Logout</button></a>
-                        <a href="<?= base_url('home') ?>"><button type="submit" class="btn">Inicio</button></a>
-                    
-                       
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-4">
-                        <div class="profile-work">
-                            <p>WORK LINK</p>
-                            <a href="">Website Link</a><br/>
-                            <a href="">Bootsnipp Profile</a><br/>
-                            <a href="">Bootply Profile</a>
-                            <p>SKILLS</p>
-                            <a href="">Web Designer</a><br/>
-                            <a href="">Web Developer</a><br/>
-                            <a href="">WordPress</a><br/>
-                            <a href="">WooCommerce</a><br/>
-                            <a href="">PHP, .Net</a><br/>
-                        </div>
-                    </div>
-                    <div class="col-md-8">
-                        <div class="tab-content profile-tab" id="myTabContent">
-                            <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
-                                        <div class="row">
-                                            <div class="col-md-6">
-                                                <label>User Id</label>
-                                            </div>
-                                            <div class="col-md-6">
-                                                <p><?= session('user')->id; ?></p>
-                                            </div>
-                                        </div>
-                                        <div class="row">
-                                            <div class="col-md-6">
-                                                <label>Name</label>
-                                            </div>
-                                            <div class="col-md-6">
-                                                <p><?= session('user')->name; ?></p>
-                                            </div>
-                                        </div>
-                                        <div class="row">
-                                            <div class="col-md-6">
-                                                <label>Email</label>
-                                            </div>
-                                            <div class="col-md-6">
-                                                <p><?= session('user')->email; ?></p>
-                                            </div>
-                                        </div>
-                                        <div class="row">
-                                            <div class="col-md-6">
-                                                <label>Phone</label>
-                                            </div>
-                                            <div class="col-md-6">
-                                                <p>123 456 7890</p>
-                                            </div>
-                                        </div>
-                                        <div class="row">
-                                            <div class="col-md-6">
-                                                <label>Profession</label>
-                                            </div>
-                                            <div class="col-md-6">
-                                                <p>Web Developer and Designer</p>
-                                            </div>
-                                        </div>
-                            </div>
-                        
-                                <div class="row">
-                                    <div class="col-md-12">
-                                        <label>Your Bio</label><br/>
-                                        <p>Your detail description</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                  
+<div class="container">
+    <div class="profile">
+        <div class="profile-header">
+            <h2>Mi Perfil</h2>
+            <button class="edit-profile-btn" onclick="enableEdit()">Editar</button>
         </div>
+        <div class="profile-details">
+            <div class="row">
+                <div class="col-6">
+                    <label for="user-id">User ID</label>
+                    <input type="text" id="user-id" value="123" disabled>
+                </div>
+                <div class="col-6">
+                    <label for="name">Nombre</label>
+                    <input type="text" id="name" value="Nombre de Usuario" disabled>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-6">
+                    <label for="email">Email</label>
+                    <input type="email" id="email" value="usuario@ejemplo.com" disabled>
+                </div>
+                <div class="col-6">
+                    <label for="phone">Teléfono</label>
+                    <input type="tel" id="phone" value="1234567890" disabled>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <label for="bio">Bio</label>
+                    <textarea id="bio" rows="4" disabled>Tu descripción detallada</textarea>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <button class="save-profile-btn" onclick="saveChanges()">Guardar Cambios</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
-    </body>
+<script>
+    function enableEdit() {
+        const inputs = document.querySelectorAll('input, textarea');
+        inputs.forEach(input => input.removeAttribute('disabled'));
+    }
+
+    function saveChanges() {
+        // Aquí puedes agregar la lógica para guardar los cambios en la base de datos
+        alert("Cambios guardados con éxito");
+    }
+</script>
+
+</body>
 </html>
--- app/Views/main/form/login.php
+++ app/Views/main/form/login.php
@@ -54,8 +54,7 @@
 						</form>
 				</div>
 
-
-
+		
 
 		</body>
 </html>
