3ba809053be08c031851b65257e7f3e4c935a605
EzequielMonteverde <cristianmonteverde@alumnos.itr3.edu.ar>
2023-10-16 22:18:25 -0300

    contact
    vista contacto y controlador modificado para que solo si estas logeado te permita hacer la consulta

--- app/Controllers/Email2.php
+++ app/Controllers/Email2.php
@@ -12,26 +12,31 @@ class Email2 extends BaseController
     }
     public function enviar__email()
     {
+        // Verificar si el usuario está autenticado
+        if (!session('user')) {
+            // Puedes personalizar este mensaje según tus necesidades
+            echo "Necesitas estar logeado para mandar una consulta";
+            return;
+        }
+
         $nombrecom = $this->request->getPost('nombrecom');
-        $emailUsuario = session('user')->email; // Obtener el correo electrónico del usuario desde la sesión
+        $emailUsuario = session('user')->email;
         $sumen = $this->request->getPost('mensaje1');
         $asuntoco = $this->request->getPost('asuntoco');
         $numtel = $this->request->getPost('numtel');
-    
+
         $email = \Config\Services::email();
-        
+
         $email->setFrom($emailUsuario);
-        $email->setTo('Keytechempresa@gmail.com'); // Utilizar el correo electrónico del usuario
-    
+        $email->setTo('Keytechempresa@gmail.com');
+
         $email->setSubject($asuntoco);
         $email->setMessage("De: " . $nombrecom . "\n" . "Numero de telefono: " . $numtel . "\n" . "Mensaje: " . $sumen);
-    
-        if (! $email->send()) {
-            echo "No se ha podido enviar el correo";    
+
+        if (!$email->send()) {
+            echo "No se ha podido enviar el correo";
         } else {
             echo "Correo enviado exitosamente";
         }
     }
-        
-            
 }
