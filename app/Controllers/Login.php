de265fbaff4f6a2dcd8427d6f6f5d634da0f88c2
EzequielMonteverde <cristianmonteverde@alumnos.itr3.edu.ar>
2023-10-30 18:05:29 -0300

    verificación de roles removido en login

--- app/Controllers/Login.php
+++ app/Controllers/Login.php
@@ -13,34 +13,29 @@ class Login extends BaseController
     }
 
     public function do_login()
-    {
-        $userModel = new UserModel();
-    
-        $email = $this->request->getPost('email');
-        $password = $this->request->getPost('password');
-        $rol = $this->request->getPost('rol');
-    
-            $result = $userModel->where('email', $email)->first();
-    
-            if ($result !== null && $result->id_user > 0) {
-                if (password_verify($password, $result->password)) {
-                echo $rol;
-                   
-                    if ($result->rol == 1) {
-                        $this->session->set("user", $result);
-                        return redirect()->to('dashboard');
-                    } else {
-                        $this->session->set("user", $result);
-                        return redirect()->to('/dashboard');
-                    }
-                } else {
-                    echo 'contraseña o email incorrecto';
-                }
-            } else {
-                echo 'contraseña o email incorrecto';
-            }
-        
+{
+    $userModel = new UserModel();
+
+    $email = $this->request->getPost('email');
+    $password = $this->request->getPost('password');
+
+    $result = $userModel->where('email', $email)->first();
+
+    if ($result !== null && $result->id_user > 0) {
+        if (password_verify($password, $result->password)) {
+            // Contraseña correcta, establece la sesión del usuario y redirige a la vista de dashboard
+            $this->session->set("user", $result);
+            return redirect()->to('dashboard');
+        } else {
+            // Contraseña incorrecta
+            echo 'Contraseña incorrecta';
+        }
+    } else {
+        // Usuario no encontrado
+        echo 'Correo electrónico no válido';
     }
+}
+
 
     public function logout()
     {
