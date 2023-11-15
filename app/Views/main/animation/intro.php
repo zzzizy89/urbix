ab52e4f1cc4bbaf8f7ed2c31796225ff1cdfb2eb
ᴄᴏᴍʙᴀ.ᴄᴏᴅᴇ <tiagocomba89@proton.me>
2023-11-15 16:54:43 -0300

    css inicio responsive

--- app/Views/main/animation/intro.php
+++ app/Views/main/animation/intro.php
@@ -4,7 +4,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>KeyTech</title>
-    <link rel="stylesheet" href="css/intro.css">
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/animation/intro.css')?>">
     <script src="https://code.jquery.com/jquery-3.4.1.js"></script>
     <script src="js/pace.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/jquery-bez@1.0.11/src/jquery.bez.js"></script>
